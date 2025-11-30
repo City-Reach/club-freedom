f4a1220b9b6a581713620d086597f553dc3313a1
Viet Hung Nguyen <vhn@sfu.ca>
2025-11-30 02:20:01 -0800

    Server-render navigation bar

--- app/routes/testimonials/route.tsx
+++ app/routes/testimonials/route.tsx
@@ -1,14 +1,22 @@
+import { getCurrentUser } from "@/app/functions/auth";
 import Navbar from "@/components/navbar";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/testimonials")({
   component: RouteComponent,
+  loader: async () => {
+    const user = await getCurrentUser();
+    return {
+      user,
+    };
+  },
 });
 
 function RouteComponent() {
+  const { user } = Route.useLoaderData();
   return (
     <>
-      <Navbar />
+      <Navbar user={user} />
       <Outlet />
     </>
   );
