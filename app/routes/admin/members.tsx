f3d765f28b070b78b0f2b3f296410a2e1f599259
Viet Hung Nguyen <vhn@sfu.ca>
2025-11-30 04:35:22 -0800

    Implement tabs for members list

--- app/routes/admin/members.tsx
+++ app/routes/admin/members.tsx
@@ -1,11 +1,19 @@
 import InviteMemberForm from "@/components/auth/invite-member-form";
-import { createFileRoute } from "@tanstack/react-router";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import z from "zod";
 
 export const Route = createFileRoute("/admin/members")({
   component: RouteComponent,
+  validateSearch: z.object({
+    status: z.enum(["active", "pending"]).default("active"),
+  }),
 });
 
 function RouteComponent() {
+  const { status } = Route.useSearch();
+  const navigate = useNavigate();
+
   return (
     <main className="flex min-h-screen flex-col py-12 px-8 gap-y-12 max-w-3xl mx-auto">
       <div className="w-full flex flex-col gap-4">
@@ -15,6 +23,30 @@ function RouteComponent() {
 
       <div className="w-full flex flex-col gap-4">
         <h2 className="text-xl font-bold">Members</h2>
+        <Tabs value={status}>
+          <TabsList>
+            <TabsTrigger
+              value="active"
+              onClick={() =>
+                navigate({ to: ".", search: { status: "active" } })
+              }
+            >
+              Active
+            </TabsTrigger>
+            <TabsTrigger
+              value="pending"
+              onClick={() =>
+                navigate({ to: ".", search: { status: "pending" } })
+              }
+            >
+              Pending
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="active">
+          </TabsContent>
+          <TabsContent value="pending">
+          </TabsContent>
+        </Tabs>
       </div>
     </main>
   );
