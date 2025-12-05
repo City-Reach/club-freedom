f1e4dd035879a4c2313a63d07be457f4755a63f5
Viet Hung Nguyen <vhn@sfu.ca>
2025-12-05 04:06:22 -0800

    Automatically accept for matching authentication

--- app/routes/invite.$invitationId.tsx
+++ app/routes/invite.$invitationId.tsx
@@ -1,26 +1,64 @@
 import AuthLayout from "@/components/layouts/auth-layout";
-import { Card, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Spinner } from "@/components/ui/spinner";
+import { api } from "@/convex/_generated/api";
 import { authClient } from "@/lib/auth/auth-client";
-import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { convexQuery } from "@convex-dev/react-query";
+import { Button } from "@react-email/components";
+import { createFileRoute, notFound, useRouter } from "@tanstack/react-router";
 import { useEffect } from "react";
 
 export const Route = createFileRoute("/invite/$invitationId")({
   component: RouteComponent,
+  notFoundComponent: NotFoundComponent,
+  loader: async ({ context, params }) => {
+    const invitation = await context.queryClient.ensureQueryData(
+      convexQuery(api.organization.findInvitationById, {
+        id: params.invitationId,
+      }),
+    );
+    if (!invitation) {
+      throw notFound();
+    }
+    return {
+      invitation,
+    };
+  },
 });
 
 function RouteComponent() {
-  const params = Route.useParams();
+  const { invitation } = Route.useLoaderData();
   const router = useRouter();
 
-  useEffect(() => {
-    authClient.signOut().then(async () => {
-      await router.invalidate();
-      await router.navigate({
-        to: "/accept-invite/$invitationId",
-        params,
-      });
+  const acceptInvitation = async () => {
+    const { data } = await authClient.organization.acceptInvitation({
+      invitationId: invitation._id,
     });
+
+    if (data) {
+      await router.navigate({
+        to: "/o/$slug",
+        params: { slug: invitation.organization.slug },
+      });
+      return;
+    }
+
+    await authClient.signOut();
+    await router.invalidate();
+    await router.navigate({
+      to: "/accept-invite/$invitationId",
+      params: { invitationId: invitation._id },
+    });
+  };
+
+  useEffect(() => {
+    acceptInvitation();
   }, []);
 
   return (
@@ -30,6 +68,21 @@ function RouteComponent() {
           <Spinner className="size-4" />
           <span>Getting invitation info</span>
         </CardContent>
+      </Card>
+    </AuthLayout>
+  );
+}
+
+function NotFoundComponent() {
+  return (
+    <AuthLayout>
+      <Card>
+        <CardHeader>
+          <CardTitle>Invitation not found</CardTitle>
+        </CardHeader>
+        <CardFooter>
+          <Button>Go Home</Button>
+        </CardFooter>
       </Card>
     </AuthLayout>
   );
