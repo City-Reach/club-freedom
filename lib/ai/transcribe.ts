796f35aafed0431de3feebb69eb81ae70816a4a1
yaacovkdev <yaacov.kochatkov@gmail.com>
2025-12-06 01:47:20 -0800

    transcribe groq integration

--- lib/ai/transcribe.ts
+++ lib/ai/transcribe.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import OpenAI from "openai";
 import Groq from "groq-sdk";
-import * as fs from "node:fs";
-import {Readable} from "node:stream";
 
 export async function transcribeAudio(upload_url: string): Promise<string> {
     let buffer: Buffer;
@@ -13,19 +11,31 @@ export async function transcribeAudio(upload_url: string): Promise<string> {
         buffer = Buffer.from(readStream.data);
 
         // @ts-ignore
-        const file = new File([buffer as Uint8Array], "audio.mp3", {
+        const audioFile = new File([buffer as Uint8Array], "audio.mp3", {
             type: "audio/mpeg"
         });
 
-        const client = new OpenAI({
-            apiKey: process.env.GROQ_API_KEY,
-            baseURL: "https://api.groq.com/openai/v1"
+        const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+        const transcription = await groq.audio.transcriptions.create({
+            file: audioFile,
+            model: "whisper-large-v3",
+            prompt: "Specify context or spelling", // Optional
+            response_format: "verbose_json", // Optional
+            timestamp_granularities: ["word", "segment"], // Optional
+            language: "en", // Optional
+            temperature: 0.0, // Optional
         });
 
-        const transcription = await client.audio.transcriptions.create({
-            file: file,
-            model: "whisper-large-v3",
-        });
+        // const client = new OpenAI({
+        //     apiKey: process.env.GROQ_API_KEY,
+        //     baseURL: "https://api.groq.com/openai/v1"
+        // });
+        //
+        // const transcription = await client.audio.transcriptions.create({
+        //     file: file,
+        //     model: "whisper-large-v3",
+        // });
+
         console.log("transcript", transcription.text);
 
         return transcription.text
