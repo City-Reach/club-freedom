87f72636279c4893862ed53836534c5883eefc5f
Viet Hung Nguyen <vhn@sfu.ca>
2025-12-09 23:54:09 -0800

    Format

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -1,10 +1,10 @@
-import { createAuthClient } from "better-auth/react";
+import { convexClient } from "@convex-dev/better-auth/client/plugins";
 import {
   adminClient,
   inferAdditionalFields,
   organizationClient,
 } from "better-auth/client/plugins";
-import { convexClient } from "@convex-dev/better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 import type { auth } from "@/convex/betterAuth/auth";
 import { adminOptions } from "./permissions";
 
