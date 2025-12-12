6bf33c4cfe03d11429a4b8c4d9504ce8a0e83725
nicktsan <nicktsang.manhay@gmail.com>
2025-12-12 02:19:29 -0800

    Changed index to use org name

--- app/routes/o.$orgSlug/index.tsx
+++ app/routes/o.$orgSlug/index.tsx
@@ -19,7 +19,7 @@ function TestimonialSubmissionPage() {
       <main className="flex min-h-screen flex-col items-center py-24 px-8 gap-y-12 max-w-3xl mx-auto">
         <div className="flex flex-col items-center justify-center text-center">
           <h1 className="text-4xl font-bold">
-            Welcome to <span className="text-secondary">Club Freedom</span>{" "}
+            Welcome to <span className="text-secondary">{organization.name}</span>{" "}
             Testimonial
           </h1>
           <p className="mt-4 text-lg">Please share your testimonial with us!</p>
