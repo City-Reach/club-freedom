bcc995e0db711efee6017c3545167c1b56d8ba77
Viet Hung Nguyen <vhn@sfu.ca>
2025-12-13 07:35:31 -0800

    Add organization logo and navbar components

---
+++ components/organization/organization-navbar.tsx
@@ -0,0 +1,33 @@
+import { Link } from "@tanstack/react-router";
+import type { Doc } from "@/convex/betterAuth/_generated/dataModel";
+import { Button } from "../ui/button";
+import UserDropDown from "../user-dropdown";
+import OrganizationLogo from "./organization-logo";
+
+type Props = {
+  user: Doc<"user"> | null;
+  organization: Doc<"organization">;
+};
+
+export default function Navbar({ user, organization }: Props) {
+  return (
+    <header className="border-b px-4 md:px-6 flex justify-between items-center sticky top-0 bg-background z-10">
+      <div className="flex items-center gap-4">
+        <OrganizationLogo organization={organization} />
+        {user && (
+          <div className="flex items-center gap-4">
+            <Button variant="link" className="cursor-pointer" asChild>
+              <Link to="/testimonials">Testimonials</Link>
+            </Button>
+          </div>
+        )}
+      </div>
+      {user && <UserDropDown user={user} />}
+      {user === null && (
+        <Button asChild>
+          <Link to="/sign-in">Sign in</Link>
+        </Button>
+      )}
+    </header>
+  );
+}
