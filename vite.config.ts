ac47d4fb89b8a47558a781f8ffb226115fb3f6eb
Viet Hung Nguyen <vhn@sfu.ca>
2025-12-22 10:34:11 -0800

    Prevent server running with bad envs

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,38 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
-    https: {
-      key: "./certificates/dev-key.pem",
-      cert: "./certificates/dev.pem",
-    },
-  },
-  plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
-    tailwindcss(),
-    tsconfigPaths(),
-    tanstackStart({
-      srcDirectory: "app",
-      router: {
-        routesDirectory: "routes",
+export default defineConfig(({ mode }) => {
+  const loadedEnv = loadEnv(mode, process.cwd(), "");
+  Object.assign(process.env, loadedEnv);
+
+  import("./env/client");
+
+  return {
+    server: {
+      port: 3000,
+      host: "0.0.0.0",
+      https: {
+        key: "./certificates/dev-key.pem",
+        cert: "./certificates/dev.pem",
       },
-    }),
-    viteReact(),
-  ],
-  build: {
-    sourcemap: true,
-  },
+    },
+    plugins: [
+      cloudflare({ viteEnvironment: { name: "ssr" } }),
+      tailwindcss(),
+      tsconfigPaths(),
+      tanstackStart({
+        srcDirectory: "app",
+        router: {
+          routesDirectory: "routes",
+        },
+      }),
+      viteReact(),
+    ],
+    build: {
+      sourcemap: true,
+    },
+  };
 });
