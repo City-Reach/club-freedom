c35b6cf545bd832a9be3e3561887a731704021e6
Viet Hung Nguyen <vhn@sfu.ca>
2025-12-27 03:33:29 -0800

    Remove unused functions

--- app/functions/auth.ts
+++ app/functions/auth.ts
@@ -1,5 +1,4 @@
 import { createServerFn } from "@tanstack/react-start";
-import z from "zod";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "@/lib/auth/auth-server";
 
@@ -7,16 +6,3 @@ export const getCurrentUser = createServerFn().handler(() => {
   const user = fetchQuery(api.auth.getCurrentUser, {});
   return user;
 });
-
-export const getUserById = createServerFn()
-  .inputValidator(
-    z.object({
-      userId: z.string(),
-    }),
-  )
-  .handler(async ({ data }) => {
-    const user = await fetchQuery(api.auth.getUserById, {
-      userId: data.userId,
-    });
-    return user;
-  });
