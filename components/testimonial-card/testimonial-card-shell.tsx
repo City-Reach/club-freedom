d45d0542bd83924689a9769c7a148bd7cc993142
yaacovkdev <yaacov.kochatkov@gmail.com>
2026-01-04 00:03:32 -0800

    clickable card to testimonial content page

--- components/testimonial-card/testimonial-card-shell.tsx
+++ components/testimonial-card/testimonial-card-shell.tsx
@@ -1,4 +1,6 @@
+import { useNavigate } from "@tanstack/react-router";
 import type { ComponentProps } from "react";
+import { useTestimonialContext } from "@/contexts/testimonial-context";
 import { cn } from "@/lib/utils";
 import { Card } from "../ui/card";
 
@@ -6,10 +8,21 @@ export default function TestimonialCardShell({
   className,
   ...props
 }: ComponentProps<typeof Card>) {
+  const navigate = useNavigate({});
+  const { testimonial } = useTestimonialContext();
+
   return (
     <Card
+      onClick={() =>
+        navigate({
+          to: "/testimonials/$id",
+          params: {
+            id: testimonial._id,
+          },
+        })
+      }
       role="link"
-      className={cn(className, "hover:bg-muted/25")}
+      className={cn(className, "hover:bg-muted/25 cursor-pointer")}
       {...props}
     />
   );
--- components/testimonial-card/testimonial-card-title.tsx
+++ components/testimonial-card/testimonial-card-title.tsx
@@ -1,14 +1,11 @@
-import { Link } from "@tanstack/react-router";
 import { useTestimonialContext } from "@/contexts/testimonial-context";
 import { CardTitle } from "../ui/card";
 
 export default function TestimonialCardTitle() {
   const { testimonial } = useTestimonialContext();
   return (
-    <Link to="/testimonials/$id" params={{ id: testimonial._id }}>
-      <CardTitle className="testimonial-card-title hover:underline">
-        {testimonial.title}
-      </CardTitle>
-    </Link>
+    <CardTitle className="testimonial-card-title hover:underline">
+      {testimonial.title}
+    </CardTitle>
   );
 }
