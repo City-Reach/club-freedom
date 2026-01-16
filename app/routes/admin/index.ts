c2cae8c9beeafb363596724762e1d77dbb9e10c3
Viet-Hung Nguyen <41867185+viethung7899@users.noreply.github.com>
2026-01-16 03:38:50 -0800

    CSR admin dashboard

--- app/routes/admin/index.ts
+++ app/routes/admin/index.ts
@@ -1,7 +1,6 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/admin/")({
-  ssr: false,
   beforeLoad: () => {
     throw redirect({
       to: "/admin/organizations",
