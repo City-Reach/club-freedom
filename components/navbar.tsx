51ad930358b0fdd2b8213a3bf049ffb5077df210
Viet-Hung Nguyen <41867185+viethung7899@users.noreply.github.com>
2026-01-16 04:34:09 -0800

    Simplify navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,21 +6,13 @@ import UserDropDown from "./user-dropdown";
 
 type Props = {
   user: Doc<"user"> | null;
-  organization?: Doc<"organization"> | undefined;
 };
 
-export default function Navbar({ user, organization }: Props) {
+export default function Navbar({ user }: Props) {
   return (
     <header className="border-b px-4 md:px-6 flex justify-between items-center sticky top-0 bg-background z-10">
       <div className="flex items-center gap-4">
-        <Logo organization={organization} />
-        {user && (
-          <div className="flex items-center gap-4">
-            <Button variant="link" className="cursor-pointer" asChild>
-              <Link to="/testimonials">Testimonials</Link>
-            </Button>
-          </div>
-        )}
+        <Logo />
       </div>
 
       {user ? (
