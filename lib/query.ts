e0d5a68542272abc9eab398511d6a969e2387a78
Viet-Hung Nguyen <41867185+viethung7899@users.noreply.github.com>
2026-01-23 16:58:28 -0800

    Use permission query from convex

--- lib/query.ts
+++ lib/query.ts
@@ -1,16 +1,7 @@
-import { queryOptions } from "@tanstack/react-query";
-import { authClient } from "@/lib/auth/auth-client";
-import type { AdminPermissionCheck } from "@/lib/auth/permissions/admin";
+import { convexQuery } from "@convex-dev/react-query";
+import { api } from "@/convex/_generated/api";
+import type { OrganizationPermissionCheck } from "./auth/permissions/organization";
 
-export function hasPermissionQuery(permissions: AdminPermissionCheck) {
-  return queryOptions({
-    queryKey: ["hasPermission", permissions],
-    queryFn: async () => {
-      const { data } = await authClient.admin.hasPermission({
-        permissions,
-      });
-      return data?.success || false;
-    },
-    staleTime: Infinity,
-  });
+export function hasPermissionQuery(permissions: OrganizationPermissionCheck) {
+  return convexQuery(api.auth.checkUserPermissions, { permissions });
 }
