ded6e342600e949ac2df167fa4cc46df95f018e9
Viet-Hung Nguyen <41867185+viethung7899@users.noreply.github.com>
2026-02-02 17:51:58 -0800

    Remove default not found

--- app/routes/o.$orgSlug/route.tsx
+++ app/routes/o.$orgSlug/route.tsx
@@ -1,11 +1,9 @@
 import { convexQuery } from "@convex-dev/react-query";
 import { createFileRoute, notFound, Outlet } from "@tanstack/react-router";
-import NotFound from "@/components/not-found";
 import { api } from "@/convex/_generated/api";
 
 export const Route = createFileRoute("/o/$orgSlug")({
   component: Component,
-  notFoundComponent: () => <NotFound />,
   beforeLoad: async ({ context, params }) => {
     const organization = await context.queryClient.ensureQueryData(
       convexQuery(api.organization.getOrganizationBySlug, {
