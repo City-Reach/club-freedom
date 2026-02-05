d61cd0fb30349803150e92020111d53fe5d7775a
nicktsan <nicktsang.manhay@gmail.com>
2026-02-05 15:27:21 -0800

    remove extra navbar from org testimonials view

--- app/routes/o.$orgSlug/_dashboard/testimonials/route.tsx
+++ app/routes/o.$orgSlug/_dashboard/testimonials/route.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { getCurrentUser } from "@/app/functions/auth";
-import Navbar from "@/components/navbar";
 
 export const Route = createFileRoute("/o/$orgSlug/_dashboard/testimonials")({
   component: RouteComponent,
@@ -16,7 +15,6 @@ function RouteComponent() {
   const { user } = Route.useLoaderData();
   return (
     <>
-      <Navbar user={user} />
       <Outlet />
     </>
   );
