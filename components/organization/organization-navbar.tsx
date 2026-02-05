6fd902976124e498257ac3806a12ad5acef672f8
Viet-Hung Nguyen <41867185+viethung7899@users.noreply.github.com>
2026-02-05 12:04:35 -0800

    Update organization logo

--- components/organization/organization-navbar.tsx
+++ components/organization/organization-navbar.tsx
@@ -13,7 +13,7 @@ export default function OrganizationNavbar() {
   return (
     <header className="border-b py-2 px-4 md:px-6 flex justify-between items-center sticky top-0 bg-background z-10">
       <div className="flex items-center gap-4">
-        <OrganizationLogo organization={organization} />
+        <OrganizationLogo />
         <div className="flex items-center">
           <Button variant="ghost" size="sm" asChild>
             <Link
